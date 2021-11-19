ba2ec25653b3a70e1057d51ba68d33285cfbaedb
Igor Chernyavsky <ic8550@gmail.com>
2021-11-20 02:30:54 +0300

    Day-1-homework-extra-task

--- js/search.js
+++ js/search.js
@@ -2,13 +2,13 @@ const search = function () {
   const input = document.querySelector('.search-block > input');
   const searchBtn = document.querySelector('.search-block > button');
 
-  input.addEventListener('input', (event) => {
-    console.log(event.target.value);
-  });
-
-  // searchBtn.addEventListener('click', (event) => {
-  //   console.log(input.value);
+  // input.addEventListener('input', (event) => {
+  //   console.log(event.target.value);
   // });
+
+  searchBtn.addEventListener('click', (event) => {
+    console.log(input.value);
+  });
 };
 
 search();
